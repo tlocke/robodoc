4d7c8ca4e500932263df68ffadc3f3a447dd5ee9
Tony Locke <tlocke@tlocke.org.uk>
2023-02-16 18:03:07 +0000

    Upgrade Scrapy

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/tlocke/robodoc",
     license="MIT",
     install_requires=[
-        "scrapy==2.7.1",
+        "scrapy==2.8.0",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
